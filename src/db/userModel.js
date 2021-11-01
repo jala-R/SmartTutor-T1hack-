32ae733f5e480f644aaff58eb19fcd6c6a532416
JALAGANDESWER.R <jalagandeswer.r@gmail.com>
2021-11-01 19:24:34 +0530

    added google auth siginup

--- src/db/userModel.js
+++ src/db/userModel.js
@@ -21,7 +21,7 @@ const userSchema=new mongoose.Schema({
     password:{
         type:String
     },
-    gmailLink:{
+    "oauth-google":{
         type:String,
         sparse:true
     }
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,9 +4,13 @@ const express=require("express"),
     UserModel=require("../db/userModel"),
     sendEmail=require("../helper/sendEmail"),
     jwt=require("jsonwebtoken"),
-    hasEmailVerifed=require("../helper/hasEmailVerified");
+    hasEmailVerifed=require("../helper/hasEmailVerified"),
+    axios=require("axios"),
+    oauthGoogleVerification=require("../helper/oauthGoogleVerification");
 
 
+
+//=====SIGINUP-PASSWORD=========================================    
 app.post("/signup/email",async (req,res)=>{
     try{
         let email=req.body.email;
@@ -30,6 +34,7 @@ app.post("/signup",hasEmailVerifed,async (req,res)=>{
         req.body.dob=new Date(`${req.body.year}-${req.body.month}-${req.body.date} 00:00.000Z`)
         let newUser=new UserModel(req.body);
         let respUser=await newUser.save();
+        res.clearCookie("esig",{path:"/signup"});
         res.send(respUser);
     }catch(err){
         res.status(400).send(err.message);
@@ -52,5 +57,74 @@ app.get("/email-verifiaction/:token",async (req,res)=>{
     }
 })
 
+//=============SIGINUP-OAUTH=============================
 
+
+//==========GOOGLE====================
+
+app.get("/signup-oauth-google",(req,res)=>{
+    let url=`https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${process.env.GOOGLECLIENTID}&scope=openid%20email%20profile&redirect_uri=${req.protocol}%3A//${req.headers.host}/signup-oath-google-callback&state=aroundTrip`
+    res.send(`<a href=${url}>google sigin</a>`);
+})
+
+app.get("/signup-oath-google-callback",(req,res)=>{
+    axios({
+        url:"https://oauth2.googleapis.com/token",
+        method:"post",
+        data:`code=${req.query.code}&client_id=${process.env.GOOGLECLIENTID}&client_secret=${process.env.GOOGLECLIENTSECRET}&redirect_uri=${req.protocol}%3A//${req.headers.host}/signup-oath-google-callback&grant_type=authorization_code`,
+  
+        headers:{
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    })
+    .then(({data})=>{
+        return axios({
+            url:"https://www.googleapis.com/oauth2/v2/userinfo",
+            method:"get",
+            headers:{
+                Authorization:`Bearer ${data.access_token}`
+            }
+        })
+    })
+    .then(({data})=>{
+        res.cookie("oagl",{id:data.id,email:data.email},{
+            httpOnly:true,
+            maxAge:1000*10*60*60,
+            signed:true,
+            path:"/signup-oath-google"
+        });
+        res.send(
+            `<form action="/signup-oath-google" method="post">
+            <label for="fname">username</label><br>
+            <input type="text" id="fname" name="username"><br><br>
+            <label for="date">date</label><br>
+            <input type="text" id="date" name="date"><br><br>
+            <label for="month">month</label><br>
+            <input type="text" id="month" name="month"><br><br>
+            <label for="year">year</label><br>
+            <input type="text" id="year" name="year"><br><br>
+            <input type="submit" value="Submit">
+          </form>`
+        );
+    })
+    .catch((err)=>{
+        res.send(err.message);
+    })
+})
+
+app.post("/signup-oath-google",oauthGoogleVerification,async (req,res)=>{
+    try{
+        req.body.email=req.signedCookies.oagl.email;
+        req.body["oauth-google"]=req.signedCookies.oagl.id;
+        req.body.dob=new Date(`${req.body.year}-${req.body.month}-${req.body.date} 00:00.000Z`);
+        let newUser=new UserModel(req.body);
+        newUser=await newUser.save();
+        res.send(newUser);
+    }catch(err){
+        res.status(400).send(err.message);
+    }
+})
+app.get("/temp",(req,res)=>{
+    console.log(req.signedCookies);
+})
 module.exports=app;
